4eaa881ffe800c929672e09e35c4983f9b93baca
MeenakshiMohan7 <meenakshimohan.ettumanoor@gmail.com>
2022-06-09 18:03:17 +0530

    Sample object array mapping

--- src/app/taskcomponent/taskcomponent.component.ts
+++ src/app/taskcomponent/taskcomponent.component.ts
@@ -10,10 +10,10 @@ export class TaskcomponentComponent implements OnInit {
   constructor() { }
   panelOpenState = false;
   tasks = [
-    "Task1",
-    "Task2",
-    "Task3",
-    "Task4"
+    { title:"Task1", summary:"This is task1", description:"1989 is the fifth studio album"},
+    { title:"Task2", summary:"This is task2", description:"by American singer-songwriter Taylor Swift"},
+    { title:"Task3", summary:"This is task3", description:"It was released on October 27, 2014"},
+    { title:"Task4", summary:"This is task4", description:"The album's synth-pop sound is characterized by heavy synthesizers,"}
   ]
   ngOnInit(): void {
   }
